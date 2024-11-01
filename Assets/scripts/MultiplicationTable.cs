62cb58cda966a73a50aa00d2dde64eba48368a7c
Mohanad-Yamin <s12326644@stu.najah.edu>
2024-11-01 23:09:39 +0200

    first commit

---
+++ Assets/scripts/MultiplicationTable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplicationTable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 1;
+        for()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
